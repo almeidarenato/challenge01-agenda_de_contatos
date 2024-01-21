792f6e4615c0bf099e64073158595ce2879b0aa6
Renato Almeida <renatoam@ymail.com>
2024-01-21 00:14:24 -0300

    adicionado funçoes de ver contatos e adicionar

--- funcoes_contatos.py
+++ funcoes_contatos.py
@@ -1,8 +1,15 @@
 def adicionar_contato(lista_de_contatos,contato):
+    lista_de_contatos.append(contato)
     print("Contato cadastrado com sucesso")
     return
 def ver_contatos(lista_de_contatos):
     print("Lista Contatos")
+    for indice,contato in enumerate(lista_de_contatos, start=1):
+        favorito = "★" if contato["favorito"] else " "
+        nome_contato = contato["nome"]
+        telefone_contato = contato["telefone"]
+        email_contato = contato["email"]
+        print(f"{indice}- [{favorito}] {nome_contato} \ntel: {telefone_contato} email:{email_contato}")
     return 
 def editar_contato(lista_de_contatos,indice_contato,contato):
     print("Contatos editado com sucesso")
