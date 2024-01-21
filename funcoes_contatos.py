fed9b62f9332febf7a6d6a5a1aa68ff96edd99f8
Renato Almeida <renatoam@ymail.com>
2024-01-21 00:17:37 -0300

    adicionado funcionalidade favoritar contato

--- funcoes_contatos.py
+++ funcoes_contatos.py
@@ -15,7 +15,9 @@ def editar_contato(lista_de_contatos,indice_contato,contato):
     print("Contatos editado com sucesso")
     return 
 def favoritar_contato(lista_de_contatos,indice_contato):
-    print("Contato favoritado com sucesso")
+    indice = int(indice_contato) -1
+    lista_de_contatos[indice]["favorito"] = True
+    print(f"Contato {indice_contato} marcado como favorito")
     return
 def apagar_contato(lista_de_contatos,contato):
     print("Contato apagado com sucesso")
