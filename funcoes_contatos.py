eb4a14532d2f5c47e79693b19901919e81eec207
Renato Almeida <renatoam@ymail.com>
2024-01-20 23:41:10 -0300

    adicionado funções do programa

---
+++ funcoes_contatos.py
@@ -0,0 +1,15 @@
+def adicionar_contato(lista_de_contatos,contato):
+    print("Contato cadastrado com sucesso")
+    return
+def ver_contatos(lista_de_contatos):
+    print("Lista Contatos")
+    return 
+def editar_contato(lista_de_contatos,indice_contato,contato):
+    print("Contatos editado com sucesso")
+    return 
+def favoritar_contato(lista_de_contatos,indice_contato):
+    print("Contato favoritado com sucesso")
+    return
+def apagar_contato(lista_de_contatos,contato):
+    print("Contato apagado com sucesso")
+    return
